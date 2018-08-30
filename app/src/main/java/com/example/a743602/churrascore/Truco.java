4268d1b17f9af04c923020e5b99022ceeeabf678
Vinicius <vinicius.carvalho@catijr.com.br>
2018-08-29 21:45:05 -0300

    Tela truco finalizada

---
+++ app/src/main/java/com/example/a743602/churrascore/Truco.java
@@ -0,0 +1,13 @@
+package com.example.a743602.churrascore;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Truco extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_truco);
+    }
+}
